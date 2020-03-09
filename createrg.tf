1a219efb8312b964478d54dde7cef198a33f64f1
gaikwadmh <52789099+gaikwadmh@users.noreply.github.com>
2020-03-09 10:32:19 +0530

    Update createrg.tf

--- createrg.tf
+++ createrg.tf
@@ -1,8 +1,32 @@
-resource "azurerm_resource_group" "rg" {
-    name     = "Mayur_tf"
-    location = "westeurope"
+# Configure the Azure provider
+provider "azurerm" { }
 
-    tags = {
-        serviceprovider = "tieto"
+resource "azurerm_resource_group" "TFDemo" {
+    name = "Mayur_POC"
+    location = "westeurope"
+}
+
+resource "azurerm_app_service_plan" "slotDemo" {
+    name                = "slotAppServicePlan"
+    location            = azurerm_resource_group.slotDemo.location
+    resource_group_name = azurerm_resource_group.slotDemo.name
+    sku {
+        tier = "Standard"
+        size = "S1"
     }
+}
+
+resource "azurerm_app_service" "slotDemo" {
+    name                = "slotAppService"
+    location            = azurerm_resource_group.slotDemo.location
+    resource_group_name = azurerm_resource_group.slotDemo.name
+    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
+}
+
+resource "azurerm_app_service_slot" "slotDemo" {
+    name                = "slotAppServiceSlotOne"
+    location            = azurerm_resource_group.slotDemo.location
+    resource_group_name = azurerm_resource_group.slotDemo.name
+    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
+    app_service_name    = azurerm_app_service.slotDemo.name
 }
